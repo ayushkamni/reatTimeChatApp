07345a54420d78d9e964f64989a1ab2bb6dd3de8
Aayush Kemni <ayushkemni8912gmail.com>
2025-08-26 18:47:21 +0530

    Initial commit

---
+++ backend/frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import {Provider} from "react-redux"
+import { store } from './redux/store.js'
+
+export const serverUrl="https://https://ayushproject-backend-f9nj.onrender.com"
+createRoot(document.getElementById('root')).render(
+<BrowserRouter>
+<Provider store={store}>
+    <App />
+</Provider>
+</BrowserRouter>
+ 
+)
